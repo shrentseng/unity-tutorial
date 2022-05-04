1168708fa3265f457a436d76af582b12df4d75f0
Shren Tseng <lspss94076@gmail.com>
2022-05-04 14:36:10 -0700

    complete level

--- unity-tutorial/Assets/Scripts/PlayerCollision.cs
+++ unity-tutorial/Assets/Scripts/PlayerCollision.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-
     public PlayerMovement movement;
    void OnCollisionEnter (Collision collisionInfo)
    {
        if (collisionInfo.collider.tag == "Obstacle")
        {
            movement.enabled = false;
+           FindObjectOfType<GameManager>().EndGame();
        }
    } 
 }
--- unity-tutorial/Assets/Scripts/PlayerMovement.cs
+++ unity-tutorial/Assets/Scripts/PlayerMovement.cs
@@ -27,5 +27,10 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(-sidewaysForce * Time.deltaTime, 0 ,0, ForceMode.VelocityChange);
         }
 
+        if (rb.position.y < 0f)
+        {
+            FindObjectOfType<GameManager>().EndGame();     
+        }
+
     }
 }
